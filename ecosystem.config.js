f4bc89ff32537a66701e3ce5558d516eebf58131
Trần Đình Nam <trandinhnamuet@gmail.com>
2025-11-28 09:50:56 +0700

    run proc in port 3002

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: 'down-detector-frontend',
+      script: 'npm',
+      args: 'start',
+      cwd: './',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3002
+      }
+    }
+  ]
+}
